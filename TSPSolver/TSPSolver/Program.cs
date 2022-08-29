5b9822e56f10a8c7fdeae0d94fa2b8d91f470ed8
DKawagishi <kawagishi.daiki@officek1.onmicrosoft.com>
2022-08-29 12:19:39 +0900

    finalize

--- TSPSolver/TSPSolver/Program.cs
+++ TSPSolver/TSPSolver/Program.cs
@@ -8,81 +8,69 @@ namespace TSPSolver
 {
     public class Christofides
     {
+        //public static List<List<double>> Main()
         public static void Main()
         {
-            List<Coord> CoordList = Samples.Sample2();
-            //bool straight = true;
-            /*List<Coord> CoordList = new List<Coord>();
-            while (true)
-            {
-                string sin = Console.ReadLine();
-                if (string.IsNullOrEmpty(sin)) break;
-                string[] s = sin.Split(' ');
-                if (s.Length == 3)
-                {
-                    int i = int.Parse(s[0]);
-                    double x = double.Parse(s[1]);
-                    double y = double.Parse(s[2]);
-                    var point = new Coord(i, x, y);
-                    CoordList.Add(point);
-                }
-            }*/
+            List<List<double>> CoordList = Samples.Sample6();
+
             int len = CoordList.Count;
-            double[,] G = new double[len,len];
-            for (int i = 0; i < len; i++) for(int j = i+1; j < len; j++) //グラフ作成
-            {
-                double dist = Coord.dist(CoordList[i], CoordList[j]);
-                G[i,j] = dist;
-                G[j,i] = dist;
-            }
-            int index = 0; //開始点
-            /*if (straight)
-            {
-                double max_dist = 0;
-                int u = 0, v = 0;
-                for (int i = 0; i < len; i++) for (int j = i + 1; j < len; j++) //グラフ作成
-                {
-                    if (max_dist < G[i, j])
-                    {
-                        max_dist = G[i, j];
-                        u = i; v = j;
-                    }
-                }
-                G[v, u] = 0;
-                G[u, v] = 0;
-                index = u;
-            }*/
+            double[,] G = make_graph(CoordList);
+            bool[,] MST = prim(len, G);
+            int[] order = loop(len, MST, G);
+            int[] path = optimize(order, G);
 
-            bool[,] MST = prim(len, G, index);
-            int[] order = loop(len, MST, G, index);
-            int[] res = three_opt(len, order, G, 100, index);
+            List<List<double>> res = new List<List<double>>();
+            for (int i = 0; i<len; i++) res.Add(new List<double> { CoordList[path[i]][0], CoordList[path[i]][1] });
 
-            for (int i = 0; i < res.Length; i++)
-            {
-                for(int j = 0; j < res.Length; j++)
-                {
-                    if (res[j] == i)
-                    {
-                        //Console.Write(i);
-                        Console.Write(" ");
-                        Console.Write(j);
-                        Console.WriteLine();
-                        break;
-                    }
-                }
-                
-            }
-
+            Console.WriteLine(total_dist(len, G));
+            Console.WriteLine(total_dist(order, G));
+            Console.WriteLine(total_dist(path, G));
             return;
+            //return res;
         }
-        
-        public static bool[,] prim(int n, double[,] G, int start_index = 0)
+        #region グラフ生成
+        private static double[,] make_graph(List<List<double>> CoordList)
+        {
+            int len = CoordList.Count();
+            double[,] G = new double[len + 2, len + 2];
+            for (int i = 0; i < len; i++) for (int j = i + 1; j < len; j++) //グラフ作成
+            {
+                double dist = calc_dist(CoordList[i][0], CoordList[i][1], CoordList[j][0], CoordList[j][1]);
+                G[i, j] = dist;
+                G[j, i] = dist;
+            }
+            return G;
+        }
+        #endregion
+
+        #region 距離計算
+        private static double calc_dist(double x1, double y1, double x2, double y2)
+        {
+            //return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+            return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+        }
+        private static double total_dist(int[] order, double[,] G)
+        {
+            double ans = 0;
+            for (int i = 0; i < order.Length - 1; i++) ans += G[order[i], order[i+1]];
+            return ans;
+        }
+        private static double total_dist(int len, double[,] G)
+        {
+            double ans = 0;
+            for (int i = 0; i < len-1; i++) ans += G[i, i+1];
+            return ans;
+        }
+        #endregion
+
+        #region 最小全域木
+        private static bool[,] prim(int n, double[,] G)
         {
             bool[,] MST = new bool[n, n];
             bool[] visited = new bool[n];
             double[] min_cost = Enumerable.Repeat(double.PositiveInfinity, n).ToArray();
             int[] min_index = Enumerable.Repeat(-1, n).ToArray();
-            min_cost[start_index] = 0;
+            min_cost[0] = 0;
             while (true)
             {
                 int v = -1; //最短距離の頂点 v
@@ -108,14 +96,16 @@ namespace TSPSolver
             }
             return MST;
         }
+        #endregion
 
-        public static int[] loop(int n, bool[,] MST, double[,] G, int start_index = 0)
+        #region 閉回路作成
+        private static int[] loop(int n, bool[,] MST, double[,] G)
         {
             List<int> list = new List<int>();
             bool[] visited = new bool[n];
             int visited_count = 0;
-            list.Add(start_index);
-            visited[start_index] = true;
+            list.Add(0);
+            visited[0] = true;
             
             void dfs(int u)
             {
@@ -140,7 +130,7 @@ namespace TSPSolver
                 }
             }
 
-            dfs(start_index);
+            dfs(0);
             visited = new bool[n];
             int[] result = new int[n];
             int index = 0;
@@ -156,87 +146,93 @@ namespace TSPSolver
             }
             return result;
         }
-        public static int[] three_opt(int n, int[] order, double[,] G, int rep = 10, int start_index = 0)
+        #endregion
+
+        #region 最適化関数
+        private static int[] optimize(int[] order, double[,] G)
         {
-            void reverse(int i, int j)
-            {
-                int[] new_order = new int[n];
-                int index = 0;
-                for (int x = 0; x < i + 1; x++) new_order[index++] = order[(start_index + x) % n];
-                for (int x = j; x >= i + 1; x--) new_order[index++] = order[(start_index + x) % n];
-                for (int x = j + 1; x < n; x++) new_order[index++] = order[(start_index + x) % n];
+            
+            int n = order.Length;
+            { //開回路の最短距離になるよう、開始点と終了点を追加する
+                int[] new_order = new int[n + 2];
+                new_order[0] = n;
+                for (int x = 0; x < n; x++) new_order[x + 1] = order[x];
+                new_order[n + 1] = n + 1;
                 order = new_order;
             }
-            void cycle(int i, int j, int k) //i→i+1→j→j+1→k→k+1 を i→j+1→k→i+1→j→k+1
-            {
-                int[] new_order = new int[n];
-                int index = 0;
-                for (int x = 0; x < i + 1; x++) new_order[index++] = order[(start_index + x) % n];
-                for (int x = j + 1; x < k + 1; x++) new_order[index++] = order[(start_index + x) % n];
-                for (int x = i + 1; x < j + 1; x++) new_order[index++] = order[(start_index + x) % n];
-                for (int x = k + 1; x < n; x++) new_order[index++] = order[(start_index + x) % n];
-            }
-            for (int _ = 0; _ < rep; _++)
-            {
-                for (int i = 0; i < n; i++)
-                {
-                    for (int j = i + 1; j < n; j++)
-                    {
-                        for (int k = j + 1; k < n; k++)
-                        {
-                            int A = order[(start_index + i) % n], B = order[(start_index + i + 1) % n], C = order[(start_index + j) % n];
-                            int D = order[(start_index + j + 1) % n], E = order[(start_index + k) % n], F = order[(start_index + k + 1) % n];
-                            double d0 = G[A, B] + G[C, D] + G[E, F];
-                            double d1 = G[A, B] + G[C, E] + G[D, F];
-                            double d2 = G[A, C] + G[B, D] + G[E, F];
-                            double d3 = G[A, C] + G[B, E] + G[D, F];
-                            double d4 = G[A, D] + G[E, B] + G[C, F];
-                            double d5 = G[A, D] + G[E, C] + G[B, F];
-                            double d6 = G[A, E] + G[D, B] + G[C, F];
-                            double d7 = G[A, E] + G[D, C] + G[B, F];
 
-                            if (d0 > d1) reverse(j, k);
-                            else if (d0 > d2) reverse(i, j);
-                            else if (d0 > d3) { reverse(i, k); cycle(i, j, k); }
-                            else if (d0 > d4) cycle(i, j, k);
-                            else if (d0 > d5) { reverse(i, j); cycle(i, j, k); }
-                            else if (d0 > d6) { reverse(j, k); cycle(i, j, k); }
-                            else if (d0 > d7) reverse(i, k);
-                        }
-                    }
-                }
+            int count = 0;
+            while (true)
+            {
+                count++;
+                double loss = three_opt(G, ref order);
+                if (loss == 0) break;
+                if (count > 1000) break;
+            }
+
+            { //開始点と終了点削除
+                int[] new_order = new int[n];
+                for (int x = 0; x < n; x++) new_order[x] = order[x + 1];
+                order = new_order;
             }
             return order;
         }
 
-    }
+        private static void reverse(int i, int j, ref int[] order)
+        {
+            int n = order.Length; 
+            int[] new_order = new int[n];
+            int index = 0;
+            for (int x = 0; x < i + 1; x++) new_order[index++] = order[x % n];
+            for (int x = j; x >= i + 1; x--) new_order[index++] = order[x % n];
+            for (int x = j + 1; x < n; x++) new_order[index++] = order[x % n];
+            order = new_order;
+        }
+        private static void cycle(int i, int j, int k, ref int[] order) //i→i+1→j→j+1→k→k+1 を i→j+1→k→i+1→j→k+1
+        {
+            int n = order.Length; 
+            int[] new_order = new int[n];
+            int index = 0;
+            for (int x = 0; x < i + 1; x++) new_order[index++] = order[x % n];
+            for (int x = j + 1; x < k + 1; x++) new_order[index++] = order[x % n];
+            for (int x = i + 1; x < j + 1; x++) new_order[index++] = order[x % n];
+            for (int x = k + 1; x < n; x++) new_order[index++] = order[x % n];
+            order = new_order;
+        }
 
-    public class Coord
-    {
-        public int index { get; set; } = 0;
-        public string Name { get; set; } = "";
-        public double X { get; set; } = 0;
-        public double Y { get; set; } = 0;
-        public Coord(Coord point)
+        private static double three_opt(double[,] G, ref int[] order)
         {
-            index = point.index; Name = point.Name; X = point.X; Y = point.Y;
+            int n = order.Length;
+            double loss = 0;
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = i + 1; j < n-1; j++)
+                {
+                    for (int k = j + 1; k < n-1; k++)
+                    {
+                        int A = order[i], B = order[i + 1], C = order[j];
+                        int D = order[j + 1], E = order[k], F = order[k + 1];
+                        double d0 = G[A, B] + G[C, D] + G[E, F];
+                        double d1 = G[A, B] + G[C, E] + G[D, F];
+                        double d2 = G[A, C] + G[B, D] + G[E, F];
+                        double d3 = G[A, C] + G[B, E] + G[D, F];
+                        double d4 = G[A, D] + G[E, B] + G[C, F];
+                        double d5 = G[A, D] + G[E, C] + G[B, F];
+                        double d6 = G[A, E] + G[D, B] + G[C, F];
+                        double d7 = G[A, E] + G[D, C] + G[B, F];
+
+                        if (d0 > d1) { reverse(j, k, ref order); loss = d0 - d1; }
+                        else if (d0 > d2) { reverse(i, j, ref order); loss = d0 - d2; }
+                        else if (d0 > d3) { reverse(i, k, ref order); cycle(i, j, k, ref order); loss = d0 - d3; }
+                        else if (d0 > d4) { cycle(i, j, k, ref order); loss = d0 - d4; }
+                        else if (d0 > d5) { reverse(i, j, ref order); cycle(i, j, k, ref order); loss = d0 - d5; }
+                        else if (d0 > d6) { reverse(j, k, ref order); cycle(i, j, k, ref order); loss = d0 - d6; }
+                        else if (d0 > d7) { reverse(i, k, ref order); loss = d0 - d7; }
+                    }
+                }
+            }
+            return loss;
         }
-        public Coord(int i, string s, double x, double y)
-        {
-            index = i; Name = s; X = x; Y = y;
-        }
-        public Coord(double x, double y)
-        {
-            X = x; Y = y;
-        }
-        public Coord(int i, double x, double y)
-        {
-            index = i; X = x; Y = y;
-        }
-        public static double dist(Coord a, Coord b)
-        {
-            double ans = (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y);
-            return ans;
-        }
+        #endregion
     }
 }
